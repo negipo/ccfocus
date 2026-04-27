9239d488f71b182abb389df247d3e0c931f2d104
Yoshiteru Negishi <negipo@gmail.com>
2026-04-27 15:58:14 +0900

    feat: register cycleNext shortcut in HotkeyController

--- ccfocus/ccfocus/HotkeyController.swift
+++ ccfocus/ccfocus/HotkeyController.swift
@@ -4,10 +4,14 @@ import KeyboardShortcuts
 @MainActor
 final class HotkeyController {
     var onToggleFocus: (() -> Void)?
+    var onCycleNext: (() -> Void)?
 
     func start() {
         KeyboardShortcuts.onKeyDown(for: .toggleFocus) { [weak self] in
             self?.onToggleFocus?()
+        }
+        KeyboardShortcuts.onKeyDown(for: .cycleNext) { [weak self] in
+            self?.onCycleNext?()
         }
     }
 }
